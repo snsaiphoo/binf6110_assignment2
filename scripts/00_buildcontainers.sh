3740f4461f1155ee55935a8e6f67fdbec2521f01
snsaiphoo <sabrinasaiphoo@gmail.com>
2026-02-18 17:36:54 -0500

    salmon.sh script

--- scripts/00_buildcontainers.sh
+++ scripts/00_buildcontainers.sh
@@ -16,3 +16,7 @@ mv sra-tools_3.2.1--h4304569_1.sif sra-toolkit.sif
 # FASTQC version 0.11.0_cv8
 singularity pull docker://biocontainers/fastqc:v0.11.9_cv8
 mv fastqc_v0.11.9_cv8.sif fastqc.sif
+
+# Salmon Quantification version 1.10.3
+singularity pull docker://combinelab/salmon:1.10.3
+mv salmon_1.10.3.sif salmon.sif
