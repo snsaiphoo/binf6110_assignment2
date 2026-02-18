1886c09100f22229884a2befc4a5c9e765b06cb5
snsaiphoo <sabrinasaiphoo@gmail.com>
2026-02-18 14:50:13 -0500

    adding containers script

---
+++ scripts/00_buildcontainers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script will contain the commands needed to obtain the .sif files needed to run the pipeline
+# Creates the containers folder to hold the .sif files
+
+cd ..
+mkdir -p containers
+cd containers
+
+module load apptainer
+
+# SRA Toolkit version 3.2.1
+singularity pull docker://quay.io/biocontainers/sra-tools:3.2.1--h4304569_1
+mv sra-tools_3.2.1--h4304569_1.sif sra-toolkit.sif
+
+# FASTQC version 0.11.0_cv8
+singularity pull docker://biocontainers/fastqc:v0.11.9_cv8
+mv fastqc_v0.11.9_cv8.sif fastqc.sif
---
+++ scripts/01_data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Move to the raw data folder
+cd ..
+mkdir -p raw_data
+cd raw_data
+
+
