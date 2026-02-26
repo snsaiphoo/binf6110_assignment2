dfe4a0ad9a639cf15c8f11b49ee93bd37c0f3b98
snsaiphoo <sabrinasaiphoo@gmail.com>
2026-02-26 15:33:58 -0500

    changes

--- scripts/08_functional_analysis.R
+++ scripts/08_functional_analysis.R
@@ -54,60 +54,42 @@ all_genes <- res_df %>%
   na.omit() %>%
   unique()
 
-# GO Enrichment 
-ego_bp <- enrichGO(gene = sig_genes,
-                   universe = all_genes,
-                   OrgDb = org.Sc.sgd.db,
-                   ont = "BP",
-                   keyType = "ORF",
-                   pAdjustMethod = "BH",
-                   pvalueCutoff = 0.05,
-                   qvalueCutoff = 0.2,
-                   readable = FALSE)
+# # GO Enrichment 
+# ego_bp <- enrichGO(gene = sig_genes,
+#                    universe = all_genes,
+#                    OrgDb = org.Sc.sgd.db,
+#                    ont = "BP",
+#                    keyType = "ORF",
+#                    pAdjustMethod = "BH",
+#                    pvalueCutoff = 0.05,
+#                    qvalueCutoff = 0.2,
+#                    readable = FALSE)
 
-png(file.path(figures_dir, "GOenrich_DP.png"),
-    width = 1000,
-    height = 800,
-    res = 150)
-
-dotplot(ego_bp, showCategory = 20,
-        title = "GO Biological Process - Stage 3 vs Stage 1")
-
-dev.off()
+# png(file.path(figures_dir, "GOenrich_DP.png"),
+#     width = 1000,
+#     height = 800,
+#     res = 150)
+# 
+# dotplot(ego_bp, showCategory = 20,
+#         title = "GO Biological Process - Stage 3 vs Stage 1")
+# 
+# dev.off()
 
 # KEGG analysis
-kegg_enrich <- enrichKEGG(gene = sig_genes,
-                          organism = 'sce',
-                          pvalueCutoff = 0.05,
-                          qvalueCutoff = 0.2)
+# kegg_enrich <- enrichKEGG(gene = sig_genes,
+#                           organism = 'sce',
+#                           pvalueCutoff = 0.05,
+#                           qvalueCutoff = 0.2)
 
-png(file.path(figures_dir, "KEGGenrich_DP.png"),
-    width = 1000,
-    height = 800,
-    res = 150)
-
-dotplot(kegg_enrich, showCategory = 15,
-        title = "KEGG Pathway Enrichment - Stage 3 vs Stage 1")
-
-dev.off()
-
-# Bar plot
-png(file.path(figures_dir, "GOenrich_BP.png"),
-    width = 1000,
-    height = 800,
-    res = 150)
-
-barplot(ego_bp, showCategory = 15, title = "GO Biological Process - Stage 3 vs Stage 1")
-
-dev.off()
-
-png(file.path(figures_dir, "KEGGenrich_BP.png"),
-    width = 1000,
-    height = 800,
-    res = 150)
-
-barplot(kegg_enrich, showCategory = 15, title = "KEGG Pathway Enrichment - Stage 3 vs Stage 1")
-dev.off()
+# png(file.path(figures_dir, "KEGGenrich_DP.png"),
+#     width = 1000,
+#     height = 800,
+#     res = 150)
+# 
+# dotplot(kegg_enrich, showCategory = 15,
+#         title = "KEGG Pathway Enrichment - Stage 3 vs Stage 1")
+# 
+# dev.off()
 
 # Comparing Upregulated and Downregulated Genes
 gene_sets <- list(
@@ -132,7 +114,8 @@ compare_go <- compareCluster(
   keyType = "ORF",
   ont = "BP",
   pAdjustMethod = "BH",
-  qvalueCutoff = 0.05
+  pvalueCutoff = 0.05,
+  qvalueCutoff = 0.2,
 )
 
 png(file.path(figures_dir, "GOenrich_upvsdown.png"),
@@ -151,7 +134,8 @@ compare_kegg <- compareCluster(
   fun = "enrichKEGG",
   organism = "sce",
   pAdjustMethod = "BH",
-  qvalueCutoff = 0.05
+  pvalueCutoff = 0.05,
+  qvalueCutoff = 0.2
 )
 
 png(file.path(figures_dir, "KEGGenrich_upvsdown.png"),
