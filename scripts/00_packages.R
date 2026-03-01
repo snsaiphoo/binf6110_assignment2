2c6ce88bf7734c9690b6d29582cf50457580f4b0
snsaiphoo <sabrinasaiphoo@gmail.com>
2026-03-01 09:40:35 -0500

    modified R packages script to include version numbers

--- scripts/00_packages.R
+++ scripts/00_packages.R
@@ -1,3 +1,24 @@
+# #########################
+# Version Numbers:
+# R 4.5.1
+# tidyr 1.3.2  
+# here 1.0.2 
+# pheatmap 1.0.13 
+# ggplot2 4.0.2 
+# dplyr 1.2.0
+# tximport 1.36.1  
+# tidyverse 2.0.0   
+# DESeq2 1.48.2
+# GenomicFeatures 1.60.0
+# org.Sc.sgd.db 3.21.0
+# AnnotationDbi 1.70.0 
+# DEGreport 1.44.0 
+# apeglm 1.30.0  
+# clusterProfiler 4.16.0
+# enrichplot 1.28.4
+# DOSE 4.2.0 
+##########################
+
 # CRAN packages
 cran_packages <- c(
   "tidyverse",
